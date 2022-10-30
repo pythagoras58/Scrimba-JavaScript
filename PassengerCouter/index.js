d202f36abac6679b51075e975c3e79b636080be6
pythagoras58 <solomondanso58@gmail.com>
2022-10-30 23:41:32 +0000

    COunter 7

--- PassengerCouter/index.js
+++ PassengerCouter/index.js
@@ -1,1 +1,8 @@
-document.getElementById('count-el').innerText = 4;
+// document.getElementById('count-el').innerText = 4;
+
+let fistBatch = 5;
+let secondBatch = 7;
+
+let count = fistBatch + secondBatch;
+
+document.getElementById('count-el').innerText = count;
